115aa2d58a368e11dab78812f2f8a8c948b64873
zee <jiseungyun@gmail.com>
2024-07-06 10:09:51 +0900

    update remove unused codes

--- Server/GameServer/Database/Common.cs
+++ Server/GameServer/Database/Common.cs
@@ -12,10 +12,5 @@ namespace Zee.Database
         };
         private static Random random = new Random();
         public static Int64 CreateUID => random.NextInt64();
-        public static MySqlConnection? connection;
-        static private readonly string connectionString = "server=zee-database-inst-1.czog0kiey03w.ap-northeast-2.rds.amazonaws.com;database=zee_database_1;uid=admin;password=dnfltkdl12";
-        public static void ConnectIfNot(this Net.ClientHandler handler)
-        {
-        }
     }
 }
